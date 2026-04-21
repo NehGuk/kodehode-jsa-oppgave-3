edad4ca07d1b0828acfd57e4d9c2a677dc5d7460
HK <hk@Skranelia-iMac.local>
2026-04-21 13:45:08 +0200

    Adds modules directory and API call

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("ohohohohoho");
+const baseUrl = "https://cmr-rest-api.fly.dev/api";
+
+const getDrinks = async () => {
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+};
+
+getDrinks();
