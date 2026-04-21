fcc3e3805cb8165a7234b78890db9645040aecc9
HK <hk@Skranelia-iMac.local>
2026-04-21 15:28:03 +0200

    Adds api call for fetching specific drink

---
+++ modules/buildDrink.js
@@ -0,0 +1,6 @@
+export const buildDrink = (content) => {
+  console.log("building drink content here blablalbalbalblaba yayyy");
+  console.log(content.name);
+  console.log(content.rating);
+  console.log(content.image);
+};
--- modules/getDrink.js
+++ modules/getDrink.js
@@ -1,6 +1,17 @@
 import { baseUrl } from "./getDrinks.js";
+import { buildDrink } from "./buildDrink.js";
+
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 const drinkUrl = `${baseUrl}${id}`;
 
-console.log(drinkUrl);
+const getDrink = async () => {
+  try {
+    const response = await fetch(drinkUrl);
+    const data = await response.json();
+    buildDrink(data);
+  } catch (error) {
+    console.error("Oops, here's an error:", error);
+  }
+};
+getDrink();
