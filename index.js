eb7e9caa4665d2e7954e7e1f37aff6c9360f49f9
NehGuk <nurture.tumulus_0n@icloud.com>
2026-04-24 13:52:35 +0200

    Refactors getDrinks.js and index.js

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 import { getDrinks } from "./modules/getDrinks.js";
-getDrinks();
+
+const mainSection = document.querySelector("#main-section");
+
+const renderDrinksPage = async () => {
+  const drinks = await getDrinks();
+
+  drinks.forEach((drink) => {
+    const drinkContainer = document.createElement("article");
+    drinkContainer.classList.add("drink-container")
+
+    const drinkImage = document.createElement("img");
+    drinkImage.src = drink.image;
+    drinkImage.alt = `Image of ${drink.name}`;
+    drinkImage.classList.add("drink-image-list");
+    
+    const drinkName = document.createElement("h3");
+    drinkName.textContent = drink.name;
+    
+    const drinkRating = document.createElement("p");
+    drinkRating.textContent = `Rating: ${drink.rating}`;
+    
+    const viewDetailsLink = document.createElement("a");
+    viewDetailsLink.href = `./drink.html?id=/${drink.id}`;
+    viewDetailsLink.classList.add("btn");
+    viewDetailsLink.textContent = "Details";
+    
+    mainSection.append(drinkContainer);
+    drinkContainer.append(drinkImage, drinkName, drinkRating, viewDetailsLink);
+  });
+}
+
+await renderDrinksPage()
--- modules/getDrinks.js
+++ modules/getDrinks.js
@@ -1,12 +1,10 @@
-import { buildDrinksList } from "./buildDrinksList.js";
-
 export const baseUrl = "https://cmr-rest-api.fly.dev/api";
 
 export const getDrinks = async () => {
   try {
     const response = await fetch(baseUrl);
     const data = await response.json();
-    buildDrinksList(data);
+    return data
   } catch (error) {
     console.error("Oops, here's an error:", error);
   }
